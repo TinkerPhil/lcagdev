3013f2ce9cb19c9304307648a3d8503ef932df7d
Richard George <rcgeorge23@gmail.com>
2019-02-07 06:56:46 +0000

    started jsp migration

--- src/main/java/uk/co/novinet/Application.java
+++ src/main/java/uk/co/novinet/Application.java
@@ -4,15 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableWebMvc
 public class Application {
     public static void main(String[] args) {
+        System.setProperty("javax.xml.transform.TransformerFactory", "com.sun.org.apache.xalan.internal.xsltc.trax.TransformerFactoryImpl");
         SpringApplication.run(Application.class, args);
     }
-
 }
--- src/main/java/uk/co/novinet/service/enquiry/PasswordSource.java
+++ src/main/java/uk/co/novinet/service/enquiry/PasswordSource.java
@@ -1,5 +1,11 @@
 package uk.co.novinet.service.enquiry;
 
+import org.apache.commons.codec.digest.Md5Crypt;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
@@ -17,5 +23,28 @@ public class PasswordSource {
         return PASSWORD_DETAILS.get(new Random().nextInt(PASSWORD_DETAILS.size()));
     }
 
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        md.update("password".getBytes());
+        String passwordHash = toHex(md.digest());
+
+        md = MessageDigest.getInstance("MD5");
+        md.update("salt".getBytes());
+        String saltHash = toHex(md.digest());
+
+        md = MessageDigest.getInstance("MD5");
+        md.update((saltHash + passwordHash).getBytes());
+        String combinedHash = toHex(md.digest());
+
+        System.out.println(combinedHash);
+    }
+
+    private static String toHex(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < bytes.length; i++) {
+            sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+        }
+        return sb.toString();
+    }
 
 }
