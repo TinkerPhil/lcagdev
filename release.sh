e2f6ccc4c997ee9e3dbd017cf02b38607e7d6a88
stratos <stratos-team@bskyb.com>
2019-04-20 10:27:54 +0100

    [RG] removing stopped containers if necessary

--- release.sh
+++ release.sh
@@ -2,7 +2,7 @@
 
 function rmContainer {
     CONTAINER_NAME=$1
-    docker rm -f $(docker ps | grep $CONTAINER_NAME | cut -c 1-12)
+    docker rm -f $(docker ps -a | grep $CONTAINER_NAME | cut -c 1-12)
 }
 
 
