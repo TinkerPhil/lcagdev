d3b854da0e8e02d85fad303e68a22f0788b61633
stratos <stratos-team@bskyb.com>
2019-04-21 08:51:53 +0100

    [RG] Now only removing containers that were started for running the app

--- remove_all.sh
+++ remove_all.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
+function rmContainer {
+    CONTAINER_NAME=$1
+    docker rm -f $(docker ps -a | grep $CONTAINER_NAME | cut -c 1-12)
+}
+
 echo "Removing all running containers"
-docker rm -f $(docker ps -a -q)
+rmContainer lcag-application ; \
+    rmContainer lcag-sftp ; \
+    rmContainer lcag-mysql ; \
+    rmContainer lcag-mail
