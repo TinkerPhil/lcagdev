5dde4d6a98ebe929c1afe11c30286f9ea65832e4
stratos <stratos-team@bskyb.com>
2019-04-22 08:54:14 +0100

    [RG] returing 0 exit code when ending remove_all.sh script

--- remove_all.sh
+++ remove_all.sh
@@ -5,8 +5,10 @@ function rmContainer {
     docker rm -f $(docker ps -a | grep $CONTAINER_NAME | cut -c 1-12)
 }
 
-echo "Removing all running containers"
+echo "Removing all containers"
 rmContainer lcag-dashboard ; \
     rmContainer lcag-sftp ; \
     rmContainer lcag-mysql ; \
     rmContainer lcag-mail
+
+exit 0
