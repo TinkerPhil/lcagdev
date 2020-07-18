967192816fb1d5af23463883e4e63b6e3b0513c1
TinkerPhil <trip@spottydog.org>
2020-07-18 21:10:10 +0200

    added user columns to table definitions

--- src/it/resources/sql/create_userFundingSummary_table.sql
+++ src/it/resources/sql/create_userFundingSummary_table.sql
@@ -5,5 +5,8 @@ CREATE TABLE IF NOT EXISTS `i7b0_userFundingSummary` (
   `isMember` bit(1) DEFAULT b'0',
   `isSuspended` bit(1) DEFAULT b'0',
   `lcagtxnCount` int(11) DEFAULT '0',
-  `ffctxnCount` int(11) DEFAULT '0'
+  `ffctxnCount` int(11) DEFAULT '0',
+  `lastPayAmnt` decimal(13,2) DEFAULT '0.00',
+  `lastPayDate` varchar(10) NULL DEFAULT 'NEVER',
+  `userStatus` varchar(10) null default 'X'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
